5344508b2b71552be9c2f6e80c3968a8eb9c4673
Boakai <boakaidkamara@gmail.com>
2022-06-21 05:48:12 +0000

    create a POST model and makemigrations

--- I4G008413HKW/blog/models.py
+++ I4G008413HKW/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+User=get_user_model()
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_date = models.DateTimeField()
+    published_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
